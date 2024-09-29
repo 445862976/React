6a5cbf9f6bf63bf94bd2bc9d2b5b2267b14c294e
Abdelrhman <abdelrhman012822@gmail.com>
2024-09-29 19:48:13 +0300

    My NAmw

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,7 +4,13 @@
 // import "./App.css";
 
 function App() {
-  return <div>Abdo Ahmed</div>;
+
+  return (
+    <div>
+      <h3>My Name</h3>
+      <h1>Abdo Ahmed</h1>
+    </div>
+  );
 }
 
 export default App;
